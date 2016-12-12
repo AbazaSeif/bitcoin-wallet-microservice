dd80c63e1e8e8a5150ee4b25fc5feeb67bd72548
rogbas <rogbas@gmail.com>
2016-12-12 10:51:52 -0200

    Basic workin of balance and transfer functions

--- server/service.js
+++ server/service.js
@@ -4,23 +4,91 @@ const express = require('express');
 const service = express();
 const config = require('../config');
 const request = require('superagent')
-const moment = require('moment');
+const bitClient = require('./bitClient');
+var bodyParser = require('body-parser');
+var feePerKb = 100e2;
+service.use(bodyParser.json());
+service.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-service.put('/service', (req, res, next) => {
 
- /*
-    request.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${req.params.location}&key=${config.geoCodingGoogleKey}`, (err, response) => {
+service.post('/service/transfer', (req, res, next) => {
+
+    console.log("Received params: ");
+    console.log(req.body);
+    var params = req.body;
+
+    if (!params.amount || !params.destination) {
+        res.json({ message: "To make a transfer you must provide a valid destination and amount" });
+        return;
+    }
+
+    console.log("Making a transfer: ");
+    console.log(params);
+
+    bitClient.createTxProposal({
+        outputs: [{
+            toAddress: params.destination,
+            amount: params.amount * 100000000,
+        }],
+        message: `automatic transfer made by the bot`,
+        type: 'external',
+        feePerKb: feePerKb
+    },
+        function (err, txp) {
+            if (err) {
+                console.log(err);
+                res.json({ message: "I got an error when creating the transfer. Sorry!" });
+                return;
+            }
+
+            bitClient.publishTxProposal({ txp: txp }, function (err, txp) {
+                if (err) {
+                    console.log(err);
+                    res.json({ message: "I got an error when publishing the transfer. Sorry!" });
+                    return;
+                }
+                bitClient.signTxProposal(txp, function (err, txp) {
+                    if (err) {
+                        console.log(err);
+                        res.json({ message: "I got an error when signing the transfer. Sorry!" });
+                        return;
+                    }
+
+                    bitClient.broadcastTxProposal(txp, function (err, txp, memo) {
+                        if (err) {
+                            console.log(err);
+                            res.json({ message: "I got an error when broadcasting the transfer. Sorry!" });
+                            return;
+                        }
+                        if (memo) {
+                            console.log(memo);
+                        };
+
+                        console.log(' * Tx created: ID %s [%s] ', txp.id, txp.status);
+                        res.json({ message: `I transfered ${params.amount} BTC to ${params.destination}` });
+
+                    });
+
+                });
+
+            });
+        });
+});
+
+service.post('/service/balance', (req, response, next) => {
+
+    console.log('Getting balance');
+    bitClient.getBalance({}, function (err, res) {
         if (err) {
             console.log(err);
-            return res.sendStatus(500);
+            return;
         }
-        //res.json(response.body.results[0].geometry.location);
-        const location = response.body.results[0].geometry.location;
-
-*/
-    console.log(res);
-
-
+        console.log(res);
+        var results = { message: `I have ${(res.totalAmount / 100000000).toFixed(6)} BTC in my balance, ${(res.lockedAmount / 100000000).toFixed(6)} BTC locked` };
+        response.json(results);
+    });
 
 });
 
